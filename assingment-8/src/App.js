25b48535c5df0af5f497f818981f6e4bbcdccef4
ADSM <yesform2018@gmail.com>
2022-03-26 19:16:14 +0600

    max-4 item select and 1 item select just one time is completed

--- assingment-8/src/App.js
+++ assingment-8/src/App.js
@@ -16,17 +16,30 @@ function App() {
   }, []);
 
   const handleAddToCart = (product) => {
-    const newCart = [...addedProduct, product];
-    setAddedProduct(newCart);
+
+    const addd = addedProduct.find(cartProduct => product.id === cartProduct.id);
+    if (addd) {
+      alert('You already select this product!')
+    }
+    else if (addedProduct.length > 3) {
+      alert('You can add only 4 items!')
+    }
+    else {
+      const newCart = [...addedProduct, product];
+      setAddedProduct(newCart);
+    }
+
   };
 
   const resetBtn = () => {
+    document.getElementById('choos-btn').style.display = 'inline';
     const newCart = [];
     setAddedProduct(newCart);
   }
 
   const choosOneBtn = () => {
 
+    document.getElementById('choos-btn').style.display = 'none';
     const randomItem = addedProduct[Math.floor(Math.random() * addedProduct.length)];
 
     const newCart = [randomItem];
@@ -49,7 +62,7 @@ function App() {
           {
             addedProduct.map((product) => <AddedProduct key={product.id} product={product}></AddedProduct>)
           }
-          <button className='choos-one-btn' onClick={choosOneBtn}>Choos one</button>
+          <button id='choos-btn' className='choos-one-btn' onClick={choosOneBtn}>Choos one</button>
           <button onClick={resetBtn} className='reset-btn'>Reset</button>
         </div>
       </div>
